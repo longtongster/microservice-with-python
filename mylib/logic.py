4244ac1876c9cd966c4f6eca80064c153320eaff
longtongster <sacha.van.weeren@gmail.com>
2025-04-02 21:18:10 +0200

    improved logic with wiki search and changed the cli

--- mylib/logic.py
+++ mylib/logic.py
@@ -5,3 +5,8 @@ def wiki(name="War Goddess", length=1):
     """This is a wikipedia fetcher"""
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search_wiki(name="Goddess"):
+    """Search Wikipedia for Names"""
+    results = wikipedia.search(name)
+    return results
